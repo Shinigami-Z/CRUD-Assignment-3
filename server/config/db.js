c3164efb7a3062d5d1a52da754564a61b83eb0dd
Shinigami-Z <yosif.iskander@ontariotechu.net>
2023-11-15 21:22:09 -0500

    Version 1.2

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,1 @@
-module.exports = {"URI":"mongodb://127.0.0.1:27017/Assignments"}
+module.exports = {"URI":"mongodb+srv://Shinigami:TaHDRpw5URPTjOXU@cluster0.ynphucg.mongodb.net/?retryWrites=true&w=majority"}
