742ec3fbd7ec2cfa7cbf17d874930ad88a9098b8
Shinigami-Z <yosif.iskander@ontariotechu.net>
2023-11-15 22:05:38 -0500

    Version 1.4

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,1 @@
-module.exports = {"URI":"mongodb+srv://Shinigami:TaHDRpw5URPTjOXU@assignments.flvftmk.mongodb.net/Assignment?retryWrites=true&w=majority"}
+module.exports = {"URI":"mongodb+srv://Shinigami:TaHDRpw5URPTjOXU@assignments.flvftmk.mongodb.net/Assignments?retryWrites=true&w=majority"}
